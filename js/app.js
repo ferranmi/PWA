d3c3099144b5aab47d92b2e872e5172853fe7daf
ferranmi <ferranmartinezivern2000@gmail.com>
2022-07-13 10:47:17 +0200

    ruta sw

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 
+var url = window.location.url;
+var swLocation = '/PWA/sw.js';
+
+
 if (navigator.serviceWorker){
-    navigator.serviceWorker.register('./sw.js');
+    if(url.includes('localhost')){
+        swLocation = '/sw.js'
+    }
+    navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
